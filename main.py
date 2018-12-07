d9dfa500092e39f748684c58f9a872c9b153d868
72ridwan <rachmat.ridwan72@gmail.com>
2018-12-08 05:49:04 +0700

    Modifikasi gitignore dan hal lain

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
 from flask import Flask, render_template, request, redirect, url_for
+import random
 app = Flask(__name__)
 
 import os
 p_antrean = "antrean"
-p_user = "user"
 
-def buat_antre_baru(pengaju):
+def generate_random_number():
+    return "".join([str(random.randint(0, 9)) for i in range (4)])
+
+def buat_antre_baru():
     daftar = os.listdir(p_antrean)
     antre_terakhir = 0
     if len(daftar) > 0:
         antre_terakhir = int(daftar[-1])
     antre_baru = str(antre_terakhir + 1).zfill(4)
+    password = generate_random_number()
     with open(p_antrean + "//" + antre_baru, "w+") as buka:
-        buka.write(pengaju)
-    return antre_baru
+        buka.write(password)
+    return (antre_baru, password)
 
-def kredensial_sesuai(username, password):
+def reset():
+    for antrean in os.listdir(p_antrean):
+        os.remove(p_antrean + "//" + antrean)
+    with open("noantre", "w") as tulis:
+        tulis.write("0000")
+
+def kredensial_sesuai(id_antre, password):
     try:
-        with open(p_user + "//" + username) as user:
+        with open(p_antrean + "//" + id_antre) as user:
             for ps in user:
-                if password == ps.split()[2]:
+                if password == ps:
                     return True
     except FileNotFoundError as e:
         pass
@@ -27,46 +37,37 @@ def kredensial_sesuai(username, password):
 
 def inkremen_no_kini():
     angka = 0
-    with open("noantri", "r") as buka:
+    with open("noantre", "r") as buka:
         for i in buka:
             angka = int(i)
             break
-    with open("noantri", "w") as tulis:
+    with open("noantre", "w") as tulis:
         tulis.write(str(angka + 1).zfill(4))
 
 def antrean_kini():
-    with open("noantri", "r") as buka:
+    with open("noantre", "r") as buka:
         for i in buka:
             return i
-
-def antrean_kini():
-    with open("noantri", "r") as buka:
-        for i in buka:
-            return i
-
-@app.route("/kirim-request", methods=['POST'])
-def kirim_request():
-    username = request.form['username']
-    password = request.form['password']
-    if kredensial_sesuai(username, password):
-        nomor_baru = buat_antre_baru(username)
-        return render_template('berhasil-request.html', no_antrenya = nomor_baru)
-    else:
-        return redirect((url_for('request_antrean')), code=302)
-        
 
 @app.route("/request")
 def request_antrean(berhasil = True):
-    return render_template('request.html', berhasil = berhasil)
+    no_antre, password = buat_antre_baru()
+    return render_template('berhasil-request.html',
+        no_antre = no_antre, password = password)
 
 @app.route("/")
 def home():
-    return render_template('home.html', noantre=antrean_kini())
+    return render_template('home.html', noantre = antrean_kini())
 
 @app.route("/inkremenkan")
 def inkremen_angka_web():
     inkremen_no_kini()
-    return render_template('home.html', noantre=antrean_kini())
+    return redirect(url_for("home"))
 
+@app.route("/reset")
+def reset_angka_web():
+    reset()
+    return redirect(url_for("home"))
+    
 if __name__ == "__main__":
     app.run(debug=True)
